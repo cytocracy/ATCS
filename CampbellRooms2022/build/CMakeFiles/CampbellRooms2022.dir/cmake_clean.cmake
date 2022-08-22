fb7c78099e547bfb08bb50c94be43df55a9b1c6b
cytocracy <theop.parker@gmail.com>
2022-08-22 10:37:59 -0700

    rooms

---
+++ CampbellRooms2022/build/CMakeFiles/CampbellRooms2022.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C:/Theo/Development/ATCS/CampbellRooms2022/CampbellRooms2022.exe"
+  "C:/Theo/Development/ATCS/CampbellRooms2022/CampbellRooms2022.exe.manifest"
+  "C:/Theo/Development/ATCS/CampbellRooms2022/CampbellRooms2022.pdb"
+  "CMakeFiles/CampbellRooms2022.dir/main.cpp.obj"
+  "CMakeFiles/CampbellRooms2022.dir/main.cpp.obj.d"
+  "libCampbellRooms2022.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CampbellRooms2022.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CampbellRooms2022/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct RoomAssignment {
+    string number;
+    string teacher;
+};
+
+vector<RoomAssignment> assignments;
+
+void loadRooms(){
+    ifstream f("rooms.txt");
+    while (!f.eof()) {
+        string num;
+        string who;
+        f >> num;
+        getline(f, who);
+        RoomAssignment ra;
+        ra.number = num;
+        ra.teacher = who.substr(1);
+        assignments.push_back(ra);
+    }
+    f.close();
+}
+
+int getChoice() {
+    cout << endl;
+    cout << "1: Search by room" << endl;
+    cout << "2: Search by teacher" << endl;
+    cout << "3: Quit" << endl;
+
+    while(true) {
+        cout << "Your choice? ";
+        int num;
+        cin >> num;
+        cin.ignore();
+        if (num >= 1 && num <= 3) {
+            return num;
+        } else return 3;
+    } 
+}
+
+void searchByRoom() {
+    cout << "Room" << endl;
+}
+
+void searchByTeacher() {
+    string who;
+    cout << "Enter the name of a teacher: ";
+    getline(cin, who);
+    for (RoomAssignment r : assignments) {
+        if (r.teacher == who) {
+            cout << r.number << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    loadRooms();
+
+    cout << "Hello!" << endl;
+    while (true) {
+        int choice = getChoice();
+        if(choice == 1) searchByRoom();
+        else if(choice == 2) searchByTeacher();
+        else break;
+    }
+    cout << "Goodbye!" << endl;
+    return 0;
+}

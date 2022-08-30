366c80a7fb5f4a8a35a631365232342ce1ed3a24
cytocracy <theop.parker@gmail.com>
2022-08-30 14:34:36 -0700

    assassin

---
+++ Assassin/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+struct DNode {
+    string name;
+    int numKills;
+    DNode* next;
+    DNode* prev;
+};
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}

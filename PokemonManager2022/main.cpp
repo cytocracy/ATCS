103f49fbd689589a43310d3a9a608da344d4c53e
cytocracy <theop.parker@gmail.com>
2022-09-06 09:59:24 -0700

    pokemon progress

--- PokemonManager2022/main.cpp
+++ PokemonManager2022/main.cpp
@@ -10,6 +10,15 @@ struct Node {
     string name;
     int num;
     Node *next;
+
+    Node(string name, int num){
+        this->name = name;
+        this->num = num;
+        next = nullptr;
+    }
+
+    Node(){}
+
 };
 
 // just a suggestion: I found it useful to have this function
@@ -17,7 +26,39 @@ struct Node {
 // if that Pokemon is not in the list anywhere
 Node* search(string name);
 
+
+
+Node* loadPokemon(){
+    ifstream file;
+    file.open("shortlist.txt");
+    Node* list = nullptr;
+    while(!file.eof()){
+        string line;
+        string name;
+        int num;
+
+        getline(file, name);
+        num = stoi(name.substr(0,name.find(" ")));
+        name = name.substr(name.find(" ")+1);
+
+        cout << name << " " << num <<  endl;
+        Node* temp = new Node(name, num);
+        temp->next = list;
+        list = temp;
+    }
+    file.close();
+    return list;
+}
+
 int main() {
+
+    Node* pokemon = loadPokemon();
+
+
+    ofstream f("pokemon.txt");
+
+
+
     cout << "Hello world" << endl;
 
     return 0;

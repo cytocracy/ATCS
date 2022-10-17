4dd98a349d7ec9551beb7ed2575421aacc21312b
cytocracy <theop.parker@gmail.com>
2022-10-17 10:35:27 -0700

    city game

---
+++ CityGuessingGame2022/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct TreeNode {
+    string value;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(string val) {
+        value = val;
+        left = nullptr;
+        right = nullptr;
+    }
+    TreeNode() {
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+int main() {
+    cout << "City Guessing Game" << endl << endl;
+    cout << "Welcome to the city City Guessing Game!" << endl;
+    string filename;
+    cout << "Enter the name of the data file: ";
+    getline(cin, filename);
+    TreeNode* root = nullptr;
+    ifstream fin(filename.c_str());
+
+    // continue here
+
+    cout << "Good bye!" << endl;
+
+    return 0;
+}

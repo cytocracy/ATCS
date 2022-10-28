89836f20556a925ceff4e91af2889476baeb3f4c
cytocracy <theop.parker@gmail.com>
2022-10-27 21:02:37 -0700

    cities done

--- CityGuessingGame2022/main.cpp
+++ CityGuessingGame2022/main.cpp
@@ -19,14 +19,19 @@ struct TreeNode {
     }
 };
 
+bool isCity(TreeNode *node){
+    if (node == nullptr) return false;
+    if (node->left == nullptr && node->right == nullptr) return true;
+    return false;
+}
 
 void buildTree(ifstream &fin, TreeNode* &root){
     string line;
     getline(fin, line);
     if (line.empty()) return;
     string type = line.substr(0, 1);
-    string value = line.substr(2);
-    root = new TreeNode(line);
+    string value = line.substr(1);
+    root = new TreeNode(value);
     if (type == "Q"){
         buildTree(fin, root->left);
         buildTree(fin, root->right);
@@ -40,74 +45,97 @@ void printTree(TreeNode* root){
     printTree(root->right);
 }
 
-void newCity(TreeNode* lastQ){
-    cout << "I give up. What is it? ";
+void newCity(TreeNode* guess){
+    cout << "Darn. What was your city?" << endl;
     string newCity;
     getline(cin, newCity);
-    cout << "Please give me a question whose answer is yes for " << newCity << " and no for " << value << ": ";
+    cout << "Please enter a question that would distinguish between" << guess->value<< " and " << newCity << ": ";
     string newQuestion;
     getline(cin, newQuestion);
-    cout << "And what is the answer for " << newCity << "? (y/n)? ";
+    cout << "If you were thinking of " << newCity << ", what would the answer be? (y/n): ";
     string newAnswer;
     getline(cin, newAnswer);
-    TreeNode* newCityNode = new TreeNode("A " + newCity);
-    TreeNode* newQuestionNode = new TreeNode("Q " + newQuestion);
     if (newAnswer == "y"){
-        newQuestionNode->left = newCityNode;
-        newQuestionNode->right = current;
+        guess->left = new TreeNode(newCity);
+        guess->right = new TreeNode(guess->value);
     } else {
-        newQuestionNode->left = current;
-        newQuestionNode->right = newCityNode;
+        guess->left = new TreeNode(guess->value);
+        guess->right = new TreeNode(newCity);
     }
-    current->value = value;
-    current = newQuestionNode;
+    guess->value = newQuestion;
 }
 
+void gameLoop(TreeNode* current){
+    while(!isCity(current)){
+        cout << current->value << " (y/n)? ";
+        string answer;
+        getline(cin, answer);
+        if (answer == "y"){
+            current = current->left;
+        } else {
+            current = current->right;
+        }
+    }
+    cout << "Is it " << current->value<< "? (y/n)? ";
+    string answer;
+    getline(cin, answer);
+    if (answer == "y"){
+        cout << "I win!" << endl;
+    } else newCity(current);
+}
+
+void saveDataHelper(TreeNode* root, ofstream &fout){
+    if (root == nullptr) return;
+    if (isCity(root)){
+        fout << "A" << root->value << endl;
+    } else {
+        fout << "Q" << root->value << endl;
+        saveDataHelper(root->left, fout);
+        saveDataHelper(root->right, fout);
+    }
+}
+
+void saveData(TreeNode* root){
+    cout << "Do you want to save the current city information to a file? (y/n): ";
+    string answer;
+    getline(cin, answer);
+    if (answer == "y"){
+        cout << "Please enter the filename: ";
+        string filename;
+        getline(cin, filename);
+        ofstream fout(filename);
+        if (fout.fail()){
+            cout << "Error opening file" << endl;
+            return;
+        }
+        saveDataHelper(root, fout);
+    }
+}
 
 int main() {
     cout << "City Guessing Game" << endl << endl;
-    cout << "Welcome to the city City Guessing Game!" << endl;
+    cout << "Welcome to the City Guessing Game!" << endl;
     string filename;
     cout << "Enter the name of the data file: ";
     getline(cin, filename);
     TreeNode* root = nullptr;
     ifstream fin(filename.c_str());
-//    string rootValue;
-//    getline(fin, rootValue);
-//    root = new TreeNode(rootValue.substr(1));
-    buildTree(fin, root);
-//    printTree(root);
 
-    TreeNode* current = root;
+    buildTree(fin, root);
+
     bool playing = true;
     while(playing){
         cout << "Think of a city. Press return when you are ready...";
         cin.get();
-        string type = current->value.substr(0, 1);
-        string value = current->value.substr(1);
-        if (type == "Q"){
-            cout << value << " (y/n)? ";
-            string answer;
-            getline(cin, answer);
-            if (answer == "y"){
-                current = current->left;
-            } else {
-                current = current->right;
-            }
-        } else {
-            cout << "Is it " << value << "? (y/n)? ";
-            string answer;
-            getline(cin, answer);
-            if (answer == "y"){
-                cout << "I win!" << endl;
-            } else {
-                newCity();
-            }
-        }
+        gameLoop(root);
+        cout << "Do you want to play again? (y/n): ";
+        string answer;
+        getline(cin, answer);
+        if (answer == "n") playing = false;
     }
 
+    saveData(root);
 
     cout << "Good bye!" << endl;
-
     return 0;
 }
